55c9fa3d7814868cf304948ff3174302c84d0f40
ShoshieCoder <46136018+ShoshieCoder@users.noreply.github.com>
2019-03-06 01:14:23 +0200

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Customer:IComparable<Customer>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public string Address { get; set; }
+        public int Protection { get; set; }
+        public int TotalPurchases { get; set; }
+
+        public Customer(int id, string name, int birthYear, string address, int protection, int totalPurchases)
+        {
+            Id = id;
+            Name = name;
+            BirthYear = birthYear;
+            Address = address;
+            Protection = protection;
+        }
+
+        public override string ToString()
+        {
+            return $"ID:{this.Id}, Name:{this.Name}, Birth year:{this.BirthYear}, Address{this.Address}, Protection:{this.Protection}";
+        }
+
+        public int CompareTo(Customer other)
+        {
+            return this.Id - other.Id;
+        }
+    }
+
+
+    class SortByProtection : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {
+            return x.Protection - y.Protection;
+        }
+    }
+
+    class SortByTotalPurchases : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {
+            return x.TotalPurchases - y.TotalPurchases;
+        }
+    }
+
+    class SortByBirthYear : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {
+            return -1*(x.BirthYear - y.BirthYear);
+        }
+    }
+}
---
+++ MyQueue.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MyQueue
+    {
+        private List<Customer> _customers = new List<Customer>();
+
+        public void Enqueue(Customer customer)
+        {
+            _customers.Insert(0, customer);
+        }
+
+        public MyQueue()
+        {
+            
+        }
+
+        public Customer Dequeue()
+        {
+            if (_customers.Count > 0)
+            {
+                Customer result = _customers[0];
+                _customers.RemoveAt(0);
+                return result;
+            }
+            return null;
+        }
+
+        public void Init(List<Customer> newList)
+        {
+            _customers = newList;
+        }
+
+        public void Clear()
+        {
+            _customers.Clear();
+        }
+
+        public Customer WhoIsNext()
+        {
+            return _customers[0];
+        }
+
+        public int count()
+        {
+            return _customers.Count;
+        }
+
+        public void SortByProtection()
+        {
+            _customers.Sort(new SortByProtection());
+        }
+
+        public void SortByTotalPurchases()
+        {
+            _customers.Sort(new SortByTotalPurchases());
+        }
+
+        public void SortByBirthYear()
+        {
+            _customers.Sort(new SortByBirthYear());
+        }
+
+        public List<Customer> DequeueCustomer(int ind)
+        {
+            if (_customers.Count > 0)
+            {
+                List<Customer> result = new List<Customer>();
+                for (int i = 0; i < ind; i++)
+                {
+                    result[i] = _customers[i];
+                    _customers.RemoveAt(i);
+                }
+                return result;
+            }
+            return null;
+        }
+
+        public void AniRakSheela(Customer customer)
+        {
+            
+        }
+    }
+}
